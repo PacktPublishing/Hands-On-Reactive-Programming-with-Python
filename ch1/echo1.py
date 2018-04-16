25c02bf77240054e3355e55b49776cfca6c2b43b
Romain Picard <romain.picard@oakbits.com>
2018-04-16 23:25:25 +0200

    ch1: added first example

---
+++ ch1/echo1.py
@@ -0,0 +1,9 @@
+import sys
+from rx import Observable
+
+argv = Observable.from_(sys.argv[1:])
+
+argv.subscribe(
+    on_next=lambda i: print("on_next: {}".format(i)),
+    on_error=lambda e: print("on_error: {}".format(e)),
+    on_completed=lambda: print("on_completed"))
